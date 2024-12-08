ce5c44102aaf22bff53b45b799a44081cc314b13
Eunji <hyni03@naver.com>
2024-12-08 13:37:46 +0900

    Feat: update /daily_count and /overall_risk to use SSE communication

--- app/routes/stats.py
+++ app/routes/stats.py
@@ -7,7 +7,12 @@ stats_bp = Blueprint('stats', __name__)
 
 @stats_bp.route('/daily_count', methods=['GET'])
 def daily_count():
-    return jsonify(get_daily_attack_count())
+    def generate():
+        while True:
+            yield f"data: {json.dumps(get_daily_attack_count())}\n\n"
+            time.sleep(60)  # 60초 간격으로 데이터 전송
+
+    return Response(generate(), content_type='text/event-stream')
 
 @stats_bp.route('/hours_count', methods=['GET'])
 def hours_attack_count():
@@ -20,4 +25,9 @@ def hours_attack_count():
 
 @stats_bp.route('/overall_risk', methods=['GET'])
 def overall_risk():
-    return jsonify(get_overall_risk())
+    def generate():
+        while True:
+            yield f"data: {json.dumps(get_overall_risk())}\n\n"
+            time.sleep(60)  # 60초 간격으로 데이터 전송
+
+    return Response(generate(), content_type='text/event-stream')
